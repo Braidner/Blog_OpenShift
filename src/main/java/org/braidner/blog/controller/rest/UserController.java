d7f7c3f3a27a845b053d586aa6c1fafffc31144d
braidner <goodly92@gmail.com>
2015-09-08 22:54:06 +0300

    Deploy

--- src/main/java/org/braidner/blog/controller/rest/UserController.java
+++ src/main/java/org/braidner/blog/controller/rest/UserController.java
@@ -5,18 +5,19 @@ import org.braidner.blog.service.SecurityService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
  * Created by Braidner on 9/8/2015.
  */
-@RestController
+@RestController("user")
 public class UserController {
 
     @Autowired
     private SecurityService securityService;
 
-    @RequestMapping
+    @RequestMapping(value = "/create", method = RequestMethod.PUT)
     public HttpStatus createUser() {
         User user = new User();
         user.setLogin("Braidner");
